e554e2b0ede425bcec422adaa8c5e1d8e4fc2890
Aevyrie Roessler <aevyrie@gmail.com>
2021-05-28 10:29:46 -0700

    Remove manual inline

--- src/lib.rs
+++ src/lib.rs
@@ -431,7 +431,6 @@ pub fn update_raycast<T: 'static + Send + Sync>(
 }
 
 /// Checks if a ray intersects a mesh, and returns the nearest intersection if one exists.
-#[inline(always)]
 fn ray_mesh_intersection(
     mesh_to_world: &Mat4,
     vertex_positions: &[[f32; 3]],
@@ -543,7 +542,6 @@ fn ray_mesh_intersection(
     pick_intersection
 }
 
-#[inline(always)]
 fn triangle_intersection(
     tri_vertices: [Vec3; 3],
     tri_normals: Option<[Vec3; 3]>,
