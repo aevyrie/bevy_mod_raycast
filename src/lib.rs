85795e46efc334cd42ea17fef5cbbd3dc9a55682
Aevyrie Roessler <aevyrie@gmail.com>
2022-08-13 16:18:20 -0700

    change to patch release and remove latent `Resource` derive

--- src/lib.rs
+++ src/lib.rs
@@ -125,7 +125,7 @@ impl<T> Clone for RaycastSystem<T> {
 }
 
 /// Global plugin state used to enable or disable all ray casting for a given type T.
-#[derive(Component, Resource)]
+#[derive(Component)]
 pub struct DefaultPluginState<T> {
     pub build_rays: bool,
     pub update_raycast: bool,
