89b1d9318fbb554c2bfca28ec9a211c8f2eba5ba
Marco Buono <thecoreh@gmail.com>
2023-05-04 02:01:53 -0300

    Properly handle non-flipped cursor coordinate system

--- src/primitives.rs
+++ src/primitives.rs
@@ -250,8 +250,10 @@ pub mod rays {
             } = camera.logical_viewport_rect()?;
             let screen_size = camera.logical_target_size()?;
             let viewport_size = viewport_max - viewport_min;
-            let adj_cursor_pos =
-                cursor_pos_screen - Vec2::new(viewport_min.x, screen_size.y - viewport_max.y);
+            let cursor_pos_screen_flipped =
+                Vec2::new(cursor_pos_screen.x, screen_size.y - cursor_pos_screen.y);
+            let adj_cursor_pos = cursor_pos_screen_flipped
+                - Vec2::new(viewport_min.x, screen_size.y - viewport_max.y);
 
             let projection = camera.projection_matrix();
             let far_ndc = projection.project_point3(Vec3::NEG_Z).z;
