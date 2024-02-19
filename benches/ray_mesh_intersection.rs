70c315e779b689a82050aafcec73a651d52d3506
Aevyrie <aevyrie@gmail.com>
2024-02-18 23:39:37 -0800

    add no culling to benches

--- benches/ray_mesh_intersection.rs
+++ benches/ray_mesh_intersection.rs
@@ -64,6 +64,30 @@ fn ray_mesh_intersection(c: &mut Criterion) {
     }
 }
 
+fn ray_mesh_intersection_no_cull(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ray_mesh_intersection_no_cull");
+    group.warm_up_time(std::time::Duration::from_millis(500));
+
+    for vertices_per_side in [10_u32, 100, 1000] {
+        group.bench_function(format!("{}_vertices", vertices_per_side.pow(2)), |b| {
+            let ray = Ray3d::new(Vec3::new(0.0, 1.0, 0.0), Vec3::new(0.0, -1.0, 0.0));
+            let mesh_to_world = Mat4::IDENTITY;
+            let mesh = mesh_creation(vertices_per_side);
+
+            b.iter(|| {
+                black_box(bevy_mod_raycast::prelude::ray_mesh_intersection(
+                    &mesh_to_world,
+                    &mesh.positions,
+                    Some(&mesh.normals),
+                    ray,
+                    Some(&mesh.indices),
+                    Backfaces::Include,
+                ));
+            });
+        });
+    }
+}
+
 fn ray_mesh_intersection_no_intersection(c: &mut Criterion) {
     let mut group = c.benchmark_group("ray_mesh_intersection_no_intersection");
     group.warm_up_time(std::time::Duration::from_millis(500));
@@ -91,6 +115,7 @@ fn ray_mesh_intersection_no_intersection(c: &mut Criterion) {
 criterion_group!(
     benches,
     ray_mesh_intersection,
+    ray_mesh_intersection_no_cull,
     ray_mesh_intersection_no_intersection
 );
 criterion_main!(benches);
